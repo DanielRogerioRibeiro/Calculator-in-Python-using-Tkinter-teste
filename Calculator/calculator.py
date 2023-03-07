3c0c370e376974707588538f5a9cbe0a2cc1bfd8
Daniel Rogerio Ribeiro <danielzule.dev@gmail.com>
2023-03-06 22:29:07 -0300

    First Commit, with the import of Tkinter and execution of the presentation window to the user.

---
+++ Calculator/calculator.py
@@ -0,0 +1,19 @@
+#Importando Tkinter
+
+from tkinter import *
+from tkinter import ttk
+
+#Colors utilizando Color picker
+
+cor1 = '#3b3b3b'  #black
+
+#Configuração da Calculador
+janela = Tk()
+janela.title ("Calculator")
+janela.geometry ("235x318")
+janela.config (bg=cor1)
+
+
+
+#Mantém a janela aberta
+janela.mainloop()
